7d9a0b25560240e124baeed6148bd8f46f68667a
Alasdair Carstairs <alasdaircarstairs@Agnes-MBP.broadband>
2019-03-04 19:40:38 +0000

    homework finished submission

--- Homework/w02_d01_hw/Extension.rb
+++ Homework/w02_d01_hw/Extension.rb
@@ -6,25 +6,19 @@ class Library
   end
 
   def get_book_by_title(title)
-    for book_hash in @books
-      if book_hash[:title] == title
-        return book_hash
-      end
-    end
+    return @books.find{|book_hash| book_hash[:title] == title}
   end
-  
+
   def get_rental_details_by_title(title)
-    for book_hash in @books
-      if book_hash[:title] == title
-        return book_hash[:rental_details]
-      end
-    end
+    return get_book_by_title(title)[:rental_details]
   end
-  #
-  # def add_book_by_title(title)
-  #   @books.push(title)
-  #   @books[:rental_details] => {:student_name => "", :date => ""}
-  #   end
-  # end
+
+  def add_book_by_title(title)
+    @books.push(:title => title, :rental_details => {:student_name => "", :date => ""})
+  end
+
+  def change_rental_details(title, student, date)
+    get_rental_details_by_title(title).replace({:student_name => student, :date => date})
+  end
 
 end
--- Homework/w02_d01_hw/specs/Extension_spec.rb
+++ Homework/w02_d01_hw/specs/Extension_spec.rb
@@ -46,4 +46,37 @@ class TestLibrary < MiniTest::Test
       }
       } , book )
     end
+
+    def test_get_rental_details_by_title
+      library = Library.new(@books)
+      book = library.get_rental_details_by_title("Dune")
+      assert_equal({
+          student_name: "Clive Johnson",
+          date: "05/05/18"
+        } , book )
+      end
+
+      def test_add_book_by_title
+        library = Library.new(@books)
+        library.add_book_by_title("Hash Potter and the Code of Secrets")
+        assert_equal({
+          title: "Hash Potter and the Code of Secrets",
+          rental_details: {
+            student_name: "",
+            date: ""
+          }
+          } , @books.last )
+      end
+
+      def test_change_rental_details
+        library = Library.new(@books)
+        library.change_rental_details("Catcher_in_the_rye","Johnny Bravo","04/03/19")
+        assert_equal({
+          title: "Catcher_in_the_rye",
+          rental_details: {
+            student_name: "Johnny Bravo",
+            date: "04/03/19"
+          }
+          } , @books[2] )
+      end
   end
