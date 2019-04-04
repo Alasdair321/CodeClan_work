8699f70e69238312daec28e403bdf6d7804b85cf
Alasdair Carstairs <alasdaircarstairs@Agnes-MacBook-Pro.local>
2019-04-04 09:08:12 +0100

    homework attempt

--- Homework/w06_d03_hw/hw_advanced_enumeration_start/isogram_finder/isogram_finder.js
+++ Homework/w06_d03_hw/hw_advanced_enumeration_start/isogram_finder/isogram_finder.js
@@ -1,9 +1,14 @@
-const IsogramFinder = function (word) {
-
+const IsogramFinder = function(word) {
+  this.word = word;
 }
 
-IsogramFinder.prototype.isIsogram = function () {
-
+IsogramFinder.prototype.isIsogram = function() {
+const result = this.word.every(this.letterReccurs);
+return result;
 }
+
+IsogramFinder.prototype.letterReccurs = function (letter) {
+  return this.word.includes(letter);
+};
 
 module.exports = IsogramFinder;
--- Homework/w06_d03_hw/hw_advanced_enumeration_start/pangram_finder/pangram_finder.js
+++ Homework/w06_d03_hw/hw_advanced_enumeration_start/pangram_finder/pangram_finder.js
@@ -1,9 +1,28 @@
-const PangramFinder = function (phrase) {
+const PangramFinder = function(phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase;
 }
 
-PangramFinder.prototype.isPangram = function () {
+PangramFinder.prototype.isPangram = function() {
+  const removedChars = this.removeSpecials();
+  const lowerCase = removedChars.toLowerCase();
+  return this.alphabet.every(this.hasLetter);
+}
 
+PangramFinder.prototype.removeSpecials = function() {};
+const lower = this.phrase.toLowerCase();
+const upper = this.phrase.toUpperCase();
+
+let result = "";
+for (let i = 0; i < lower.length; ++i) {
+  if (lower[i] != upper[i] || lower[i].trim() === '')
+    result += this.phrase[i];
+}
+return result;
+}
+
+PangramFinder.prototype.hasLetter = function(letter) {
+  return this.phrase.includes(letter);
 }
 
 module.exports = PangramFinder;
