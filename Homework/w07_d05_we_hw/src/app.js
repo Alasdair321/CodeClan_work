bce04ded659339653ea8041fa3b9b58475ef11c6
Alasdair Carstairs <alasdaircarstairs@Agnes-MBP.broadband>
2019-04-15 06:09:30 +0100

    got coordinates on click on map

--- Homework/w07_d05_we_hw/src/app.js
+++ Homework/w07_d05_we_hw/src/app.js
@@ -7,15 +7,11 @@ const CrimeFinder = require('./models/CrimeFinder.js');
 document.addEventListener('DOMContentLoaded',()=>{
   console.log(`js loaded`);
 
-  var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      maxZoom: 18,
-      id: 'mapbox.streets',
-      accessToken: 'pk.eyJ1IjoiYWxhc2RhaXIzMjEiLCJhIjoiY2p1aHRtc3gyMDM4bDRlcGVoY2YwZzFwZyJ9.bOkojmu7l5VbM5zdlSxQhA'
-  }).addTo(mymap);
-  var marker = L.marker([51.5, -0.09]).addTo(mymap);
-  marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+  const map = new MapView(L.map('mapid').setView([51.505, -0.09], 13));
+  map.generate();
+  // map.bindEvents();
+
+  
 
 
 });
--- Homework/w07_d05_we_hw/src/views/MapView.js
+++ Homework/w07_d05_we_hw/src/views/MapView.js
@@ -1,15 +1,30 @@
-const MapView = function (){
+const PubSub
+
+const MapView = function (mymap){
+    this.mymap = mymap;
+};
+
+MapView.prototype.generate = function (){
+    // var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1IjoiYWxhc2RhaXIzMjEiLCJhIjoiY2p1aHRtc3gyMDM4bDRlcGVoY2YwZzFwZyJ9.bOkojmu7l5VbM5zdlSxQhA'
+    }).addTo(this.mymap);
+    var marker = L.marker([51.5, -0.09]).addTo(this.mymap);
+    marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+    this.mymap.on('click', this.onMapClick);
 
 };
 
-// 
-// var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-//
-// L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     id: 'mapbox.streets',
-//     accessToken: 'your.mapbox.access.token'
-// }).addTo(mymap);
+// MapView.prototype.bindEvents = function(){
+// };
+
+MapView.prototype.onMapClick = function (e) {
+    // alert("You clicked the map at " + e.latlng);
+    console.log(e.latlng);
+    
+}
 
 module.exports = MapView;
